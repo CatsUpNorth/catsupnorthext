f7b81ca9e015a0a6e1e24f611d89f58e1f508a72
larry <larry.giroux@outlook.com>
2024-09-11 22:52:50 -0400

    Testing yaml metadata idea.

--- background.js
+++ background.js
@@ -1,5 +1,48 @@
 let windowId;
 
+var msstrat = {
+  sites: {},
+  strats: {},
+  use: {
+    title: [
+      'title@innerText',
+      'meta[property="og:title"]',
+      'meta[name="twitter:title"]'
+    ],
+    description: [
+      'meta[name="description"]',
+      'meta[property="og:description"]',
+      'meta[name="twitter:description"]'
+    ],
+    author: [
+      'meta[name="author"]',
+      'ytd-video-owner-renderer .ytd-channel-name@innerText',
+      '.ytd-video-owner-renderer .ytd-channel-name@innerText'
+    ],
+    favicon: [
+      'link[rel="icon"]@href',
+      'link[rel="shortcut icon"]@href',
+      'link[rel="apple-touch-icon"]@href'
+    ],
+    date: [
+      'meta[name="date"]',
+      'meta[property="article:published_time"]',
+      'meta[property="article:modified_time"]',
+      'meta[property="og:updated_time"]',
+      'meta[property="og:published_time"]'
+    ],
+    image: [
+      'meta[property="og:image"]',
+      'meta[name="twitter:image"]',
+      'meta[name="twitter:card"]',
+      'link[rel="preload"]@href'
+    ],
+    language: [
+      'html@lang'
+    ]
+  }
+}
+
 function logTabUrl(tabId) {
   chrome.tabs.get(tabId, (tab) => {
     if (tab && tab.url && !tab.url.startsWith('chrome://') && !tab.url.startsWith('file://')) {
@@ -8,24 +51,56 @@ function logTabUrl(tabId) {
         target: { tabId: tabId },
         func: () => {
           // Extract metadata from the page
-          const metadata = {
-            title: document.querySelector('meta[property="og:title"]')?.content ||
-                   document.querySelector('title')?.innerText || 
-                   '',
-            description: document.querySelector('meta[name="description"]')?.content ||
-                         document.querySelector('meta[property="og:description"]')?.content ||
-                         '',
-            author: document.querySelector('meta[name="author"]')?.content || 
-                    '',
-            favicon: document.querySelector('link[rel="icon"]')?.href ||
-                     document.querySelector('link[rel="shortcut icon"]')?.href || 
-                     '',
-            datePublished: document.querySelector('meta[property="article:published_time"]')?.content || 
-                           '',
-            image: document.querySelector('meta[property="og:image"]')?.content || 
-                   '',
-            language: document.querySelector('html')?.lang ||  '',
-          };
+          var metadata = {};
+          for(const prop in msstrat.use){
+            const prop_obj = msstrat.use[prop];
+            for(var i = 0; i < prop_obj.length; i++){
+              if(metadata[prop] && typeof metadata[prop] == 'string' && metadata[prop].length) break;
+              const strat_str = prop_obj[i];
+              var [selector_preference, attr] = strat_str.split('@');
+              var [selector, preference] = selector_preference.split('!');
+              var el = document.querySelector(selector); // default to first element
+              switch(preference){
+                case 'widest':
+                  // Loop through all elements and set el to the widest one
+                  var widest = 0;
+                  var els = document.querySelectorAll(selector);
+                  for(var j = 0; j < els.length; j++){
+                    if(els[j].offsetWidth > widest){
+                      widest = els[j].offsetWidth;
+                      el = els[j];
+                    }
+                  }
+                case 'tallest':
+                  // Loop through all elements and set el to the tallest one
+                  var tallest = 0;
+                  var els = document.querySelectorAll(selector);
+                  for(var j = 0; j < els.length; j++){
+                    if(els[j].offsetHeight > tallest){
+                      tallest = els[j].offsetHeight;
+                      el = els[j];
+                    }
+                  }
+                default:;
+              }
+              switch(attr){
+                case 'innerText':
+                  metadata[prop] = el?.innerText;
+                  break;
+                case 'href':
+                  metadata[prop] = el?.href;
+                  break;
+                case 'src':
+                  metadata[prop] = el?.src;
+                  break;
+                case 'lang':
+                  metadata[prop] = el?.lang;
+                  break;
+                default:
+                  metadata[prop] = el?.content;
+              }
+            }
+          }
           return metadata;
         }
       }, (results) => {
